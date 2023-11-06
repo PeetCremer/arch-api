7fe9be40642b69c9448155059dcb1b6b2449a7c1
Peet Cremer <peet.cremer@gmail.com>
2023-11-06 01:58:08 +0100

    Update run_prod_stack.sh to be more self-contained for GCP

--- run_prod_stack.sh
+++ run_prod_stack.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker-compose -f docker-compose-prod.yml up -d
+set -e
+sudo gcloud auth print-access-token | sudo docker login -u oauth2accesstoken --password-stdin europe-west1-docker.pkg.dev
+sudo docker pull europe-west1-docker.pkg.dev/arch-api-403919/arch-api-registry/arch-api:main
+docker compose -f docker-compose-prod.yml up -d
