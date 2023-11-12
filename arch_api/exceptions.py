5e5dcdf2845c13c9f865a676d78472122dabeb4a
Peet Cremer <peet.cremer@gmail.com>
2023-11-12 18:20:31 +0100

    docstrings for exceptions

--- arch_api/exceptions.py
+++ arch_api/exceptions.py
@@ -3,10 +3,17 @@ from bson.errors import InvalidId
 
 
 class SplittingError(Exception):
+    """
+    Error originating from geometries that cannot be split
+    """
+
     ...
 
 
 async def splitting_error_handler(_: fastapi.Request, exc: SplittingError) -> fastapi.responses.JSONResponse:
+    """
+    Transforms a SplittingError into a BAD_REQUEST response
+    """
     return fastapi.responses.JSONResponse(
         status_code=fastapi.status.HTTP_400_BAD_REQUEST,
         content={"detail": str(exc)},
@@ -14,6 +21,9 @@ async def splitting_error_handler(_: fastapi.Request, exc: SplittingError) -> fa
 
 
 async def invalid_object_id_handler(_: fastapi.Request, exc: InvalidId) -> fastapi.responses.JSONResponse:
+    """
+    Transforms a bson InvalidId error into a BAD_REQUEST response
+    """
     return fastapi.responses.JSONResponse(
         status_code=fastapi.status.HTTP_400_BAD_REQUEST,
         content={"detail": str(exc)},
