ba70caef9d6533393f741224f516bcdf7324f8c0
Peet Cremer <peet.cremer@gmail.com>
2023-11-12 13:54:59 +0100

    Move geometric fixture to unittest

--- tests/conftest.py
+++ tests/conftest.py
@@ -34,34 +34,3 @@ def invalid_testcase(request: pytest.FixtureRequest) -> dict[str, Testcase]:
 @pytest.fixture
 def vaterlandsparken_testcase() -> dict[str, dict[str, Any]]:
     return load_testcase("vaterlandsparken")
-
-
-@pytest.fixture
-def building_limits(vaterlandsparken_testcase: dict[str, dict[str, Any]]) -> dict[str, Any]:
-    return vaterlandsparken_testcase["building_limits"]
-
-
-@pytest.fixture
-def height_plateaus(vaterlandsparken_testcase: dict[str, dict[str, Any]]) -> dict[str, Any]:
-    return vaterlandsparken_testcase["height_plateaus"]
-
-
-@pytest.fixture
-def feature(building_limits: dict[str, Any]) -> dict[str, Any]:
-    feature = building_limits["features"][0]
-    assert isinstance(feature, dict)
-    return feature
-
-
-@pytest.fixture
-def feature_with_elevation(height_plateaus: dict[str, Any]) -> dict[str, Any]:
-    feature = height_plateaus["features"][0]
-    assert isinstance(feature, dict)
-    return feature
-
-
-@pytest.fixture
-def polygon(feature: dict[str, Any]) -> dict[str, Any]:
-    polygon = feature["geometry"]
-    assert isinstance(polygon, dict)
-    return polygon
